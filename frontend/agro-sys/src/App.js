1eb60c00b7aac5c834ae124253549f7f96c652f4
Prajit-Sengupta <ratulsengupta8@gmail.com>
2022-06-25 18:21:00 +0530

    Added bg and form

--- frontend/agro-sys/src/App.js
+++ frontend/agro-sys/src/App.js
@@ -1,11 +1,11 @@
 import Navbar from './components/Navbar';
-
+import Landing from './components/Landing';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-
+    <Landing/>
     </div>
   );
 }
