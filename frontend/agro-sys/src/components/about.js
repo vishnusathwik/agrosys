7b50a1c9bbc37a185171a92814f909a6a38b6404
itzmishti <vgupta4_be20@thapar.edu>
2022-06-25 17:47:19 +0530

    Commit1

--- frontend/agro-sys/src/components/about.js
+++ frontend/agro-sys/src/components/about.js
@@ -2,7 +2,32 @@ import React from 'react'
 
 function about() {
   return (
-    <div>about</div>
+    <div>
+  <h2>THE AGRINET GROUP OF COMPANIES</h2>
+  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="images/lemongrass.jfif" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="images/lemongrass2.jfif" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="images/farming.jfif" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+    </div>
+
   )
 }
 
