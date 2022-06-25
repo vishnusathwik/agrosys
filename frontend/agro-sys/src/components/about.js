6414574cceabb756f0569a78e24914eb6092021e
sinchu2304 <sinchu2304@gmail.com>
2022-06-25 18:19:26 +0530

    about

--- frontend/agro-sys/src/components/about.js
+++ frontend/agro-sys/src/components/about.js
@@ -1,32 +1,40 @@
 import React from 'react'
+import Farming from './images/farming.jfif'
+import Lemongrass from './images/lemongrass.jfif'
+import Lemongrass2 from './images/lemongrass2.jfif'
+import Lemongrass3 from './images/lemongrass3.jfif'
 
 function about() {
   return (
     <div>
   <h2>THE AGRINET GROUP OF COMPANIES</h2>
-  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="images/lemongrass.jfif" class="d-block w-100" alt="...">
+  <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <img src={Farming} className="d-block w-100" alt="..."/>
       </div>
-      <div class="carousel-item">
-        <img src="images/lemongrass2.jfif" class="d-block w-100" alt="...">
+      <div className="carousel-item">
+        <img  src={Lemongrass} className="d-block w-100" alt="..."/>
       </div>
-      <div class="carousel-item">
-        <img src="images/farming.jfif" class="d-block w-100" alt="...">
+      <div className="carousel-item">
+        <img src={Lemongrass2} className="d-block w-100" alt="..."/>
+      </div>
+      <div className="carousel-item">
+        <img src={Lemongrass3} className="d-block w-100" alt="..."/>
       </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
+    <button classNameName="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
+    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
     </button>
+    </div>
   </div>
 
-    </div>
+    
 
   )
 }
